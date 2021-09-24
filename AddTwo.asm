3294461c7b32ed9882f967453950153c9fc83b35
Brian Chalfant <brian.chalfant@gmail.com>
2021-09-24 14:10:16 -0400

    First Commit

---
+++ AddTwo.asm
@@ -0,0 +1,33 @@
+; AddTwo.asm - adds two 32-bit integers.
+; Chapter 3 example
+
+.386
+.model flat,stdcall
+.stack 4096
+ExitProcess proto,dwExitCode:dword
+INCLUDE Irvine32.inc
+
+.data
+
+str1 BYTE "hello world", 0
+
+
+.code
+main proc
+	mov	eax, blue + (white * 16) ;
+	mov ecx, 17
+
+	L1: call SetTextColor
+
+	mov edx, OFFSET str1
+	call WriteString
+
+	call Crlf
+
+	add eax, 1
+
+	loop L1			
+
+	invoke ExitProcess,0
+main endp
+end main
